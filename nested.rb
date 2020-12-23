3efe576753cb21eb92adb1c2a0a8042e6eade942
Linda-Monique Nyangoh Timoh Hendricks <linda.timoh@gmail.com>
2020-12-23 16:19:58 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -38,6 +38,7 @@ def alan_kay_is_known_for
         :languages => ["C"]
       }
     }
+programmer_hash[:alan_kay ][:known_for]
 end
 
 def dennis_ritchies_language
@@ -109,8 +110,7 @@ def changing_alan
         :languages => ["C"]
       }
     }
-if changing_alan.include? :grace_hopper
-   programmer_hash[:alan_kay][:known_for] ="GUI"
+
 end
 programmer_hash
 end
